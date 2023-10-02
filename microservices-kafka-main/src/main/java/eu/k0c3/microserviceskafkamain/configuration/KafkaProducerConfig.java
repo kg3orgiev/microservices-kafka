0213d11154d339f5442a0da36d7b94ae691b3de5
Kosta Georgiev <kosta.georgiev@dreamix.eu>
2023-10-02 11:33:56 +0300

    Update kafka path

--- microservices-kafka-main/src/main/java/eu/k0c3/microserviceskafkamain/configuration/KafkaProducerConfig.java
+++ microservices-kafka-main/src/main/java/eu/k0c3/microserviceskafkamain/configuration/KafkaProducerConfig.java
@@ -1,18 +1,13 @@
 package eu.k0c3.microserviceskafkamain.configuration;
 
 import eu.k0c3.microserviceskafkamain.dtos.CarPostDTO;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Properties;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -20,18 +15,15 @@ public class KafkaProducerConfig {
     private String bootstrapServer;
 
     @Bean
-    public ProducerFactory<String, CarPostDTO> userProducerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+    public KafkaProducer<String, CarPostDTO> userProducerFactory() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username='2uwZI24oqOjvXRcEuv3f8r' password='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiIydXdaSTI0b3FPanZYUmNFdXYzZjhyIiwib3JnYW5pemF0aW9uSWQiOjc2NDExLCJ1c2VySWQiOjg4OTAxLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiIyOTEwN2Q0My1kM2VhLTRhZWItYjJiMS0wNzQ4YTgwOGY2NzgifX0.9TxtdquwD5UBfZNLYLRt9mXghFHzmeW_nx3CzLWto2I';");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, CarPostDTO> userKafkaTemplate() {
-        return new KafkaTemplate<>(userProducerFactory());
+        return new KafkaProducer<>(properties);
     }
 }
